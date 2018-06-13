2f705e15e19b23a7cc7d2794a51343121ade8b63
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-06-14 01:37:47 +0200

    mysqltest: Use environment variables

--- dbtest/mysqltest/cli.go
+++ dbtest/mysqltest/cli.go
@@ -1,25 +1,13 @@
 package mysqltest
 
-// value is the common interface used by flag/pflag/env packages.
-type value interface {
-	Set(string) error
-	Type() string
-	String() string
-}
+import "github.com/goph/env"
 
-// configurator implements the config methods required by the database config.
-type configurator interface {
-	StringVar(p *string, name string, value string, usage string)
-	IntVar(p *int, name string, value int, usage string)
-	Var(value value, name string, usage string)
-}
-
-// PrepareConfig registers configuration in the configurator.
-func PrepareConfig(c configurator, config *Config) {
-	c.StringVar(&config.Host, "db-host", "", "Database host")
-	c.IntVar(&config.Port, "db-port", 3306, "Database port")
-	c.StringVar(&config.User, "db-user", "root", "Database user")
-	c.StringVar(&config.Pass, "db-pass", "", "Database password")
-	c.StringVar(&config.Name, "db-name", "", "Database name")
-	c.Var(&config.Params, "db-params", "Database params")
+// PrepareConfig registers configuration in the EnvVarSet.
+func PrepareConfig(e *env.EnvVarSet, config *Config) {
+	e.StringVar(&config.Host, "db-host", "", "Database host")
+	e.IntVar(&config.Port, "db-port", 3306, "Database port")
+	e.StringVar(&config.User, "db-user", "root", "Database user")
+	e.StringVar(&config.Pass, "db-pass", "", "Database password")
+	e.StringVar(&config.Name, "db-name", "", "Database name")
+	e.Var(&config.Params, "db-params", "Database params")
 }
